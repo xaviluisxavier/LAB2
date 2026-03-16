4070d3df6df319e0036706ee77bf20e60ca32c4f
Luís Pacheco <30757322+xaviluisxavier@users.noreply.github.com>
2026-03-16 11:31:51 -0100

    Delete cliente directory

--- cliente/__main__.py
+++
@@ -1,9 +0,0 @@
-from cliente.interface.interface import Interface
-
-def main():
-    print("A iniciar Cliente de Cálculo...")
-    intf = Interface()
-    intf.execute()
-
-if __name__ == '__main__':
-    main()
