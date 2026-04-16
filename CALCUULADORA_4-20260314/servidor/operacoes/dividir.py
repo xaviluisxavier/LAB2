46c0d09a0f0b51caec109174764451cbf40781dc
Luís Pacheco <30757322+xaviluisxavier@users.noreply.github.com>
2026-04-16 19:01:46 +0000

    Update dividir.py

--- CALCUULADORA_4-20260314/servidor/operacoes/dividir.py
+++ CALCUULADORA_4-20260314/servidor/operacoes/dividir.py
@@ -1,11 +1,13 @@
 from typing import Union
 
 class Dividir:
+    """
+    Classe responsável pela operação matemática de divisao.
+    """
     def __init__(self):
         self.x = 0
         self.y = 0
         self.res = 0
-
 
     def execute(self)->Union[float,str]:
         try:
